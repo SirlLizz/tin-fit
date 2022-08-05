52394a3d8057c77cf57fc7b7a291ff3fddc1123b
Ilya <sirllizz@gmail.com>
2022-08-06 01:24:47 +0400

    Add ContactButton

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
@@ -7,22 +6,26 @@ import About from "./pages/About";
 import Contact from "./pages/Сontact";
 import NoMatch from "./pages/NoMatch";
 import Prices from "./pages/Prices";
+import ContactButton from "./components/ContactButton";
+import Footer from "./components/Footer";
 
 function App() {
     return (
     <div className="App">
-      <header className="App-header">
-          <Router>
-              <NavBar/>
-              <Routes>
-                  <Route exact path="/" element={<Home/>} />
-                  <Route exact path="/about" element={ <About/> } />
-                  <Route exact path="/prices" element={ <Prices/> } />
-                  <Route exact path="/contact" element={ <Contact/> } />
-                  <Route component={ NoMatch } />
-              </Routes>
-          </Router>
-      </header>
+        <header className="App-header">
+            <Router>
+                <ContactButton/>
+                <NavBar/>
+                <Routes>
+                    <Route exact path="/" element={<Home/>} />
+                      <Route exact path="/about" element={ <About/> } />
+                    <Route exact path="/prices" element={ <Prices/> } />
+                     <Route exact path="/contact" element={ <Contact/> } />
+                     <Route component={ NoMatch } />
+                </Routes>
+            </Router>
+        </header>
+        <Footer/>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,14 +5,32 @@ import './NavBar.css';
 class NavBar extends React.Component {
     render() {
         return (
-            <div className = "navbar">
-                <p className = "brand">Tin Fit</p>
-                <div className="groupbtn">
-                    <Link to="/"><span className = "navbtn">ГЛАВНАЯ</span></Link>
-                    <Link to="/about"><span className = "navbtn">О НАС</span></Link>
-                    <Link to="/prices"><span className = "navbtn">ЦЕНЫ И УСЛУГИ</span></Link>
-                    <Link to="/contact"><span className = "navbtn">КОНТАКТЫ</span></Link>
-                </div>
+            <div className= "spacer">
+                <section id="nav-bar">
+                    <nav className="navbar navbar-expand-lg navbar-light">
+                        <a className="navbar-brand" href="/">TINFIT</a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav ml-auto">
+                                <span className="nav-item">
+                                    <Link className="nav-link" to="/">ГЛАВНАЯ</Link>
+                                </span>
+                                <span className="nav-item">
+                                    <Link className="nav-link" to="/about">О НАС</Link>
+                                </span>
+                                <span className="nav-item">
+                                    <Link className="nav-link" to="/prices">ЦЕНЫ И УСЛУГИ</Link>
+                                </span>
+                                <span className="nav-item">
+                                    <Link className="nav-link" to="/contact">КОНТАКТЫ</Link>
+                                </span>
+                            </ul>
+                        </div>
+                    </nav>
+                </section>
             </div>
         );
     }
